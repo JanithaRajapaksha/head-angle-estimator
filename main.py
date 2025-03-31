d060f3983ed7cb98079c7ef97926fcd88bd9133c
JanithaRajapaksha <jlkrajapaksha@gmail.com>
2025-03-31 10:57:49 +0530

    fixed thresholds and direction

--- main.py
+++ main.py
@@ -7,30 +7,21 @@ mp_face_mesh = mp.solutions.face_mesh
 face_mesh = mp_face_mesh.FaceMesh(min_detection_confidence=0.5, min_tracking_confidence=0.5)
 
 mp_drawing = mp.solutions.drawing_utils
-
 drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
 
 cap = cv2.VideoCapture(0)
 
 while cap.isOpened():
     success, image = cap.read()
+    if not success:
+        break
 
     start = time.time()
 
-    # Flip the image horizontally for a later selfie-view display
-    # Convert the BGR image to RGB
     image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
-
-    # To improve performance, optionally mark the image as not writeable to pass by reference
     image.flags.writeable = False
-
-    # Get the result
     results = face_mesh.process(image)
-
-    # To improve performance, optionally mark the image as writeable to pass by reference
     image.flags.writeable = True
-
-    # Convert the color back to BGR
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
     img_h, img_w, img_c = image.shape
@@ -40,82 +31,52 @@ while cap.isOpened():
     if results.multi_face_landmarks:
         for face_landmarks in results.multi_face_landmarks:
             for idx, lm in enumerate(face_landmarks.landmark):
-                if idx == 33 or idx == 263 or idx == 1 or idx == 61 or idx == 291 or idx == 199:
+                if idx in [33, 263, 1, 61, 291, 199]:
                     if idx == 1:
                         nose_2d = (lm.x * img_w, lm.y * img_h)
                         nose_3d = (lm.x * img_w, lm.y * img_h, lm.z * 3000)
 
                     x, y = int(lm.x * img_w), int(lm.y * img_h)
-
-                    # Get the 2D coordinates of the face
                     face_2d.append((x, y))
-
-                    # Get the 3D coordinates of the face
                     face_3d.append((x, y, lm.z))
 
-            # Convert the face_2d to a numpy array
             face_2d = np.array(face_2d, dtype=np.float64)
-
-            # Convert the face_3d to a numpy array
             face_3d = np.array(face_3d, dtype=np.float64)
 
-            # The camera matrix
             focal_length = 1 * img_w
-
             cam_matrix = np.array([[focal_length, 0, img_w / 2],
                                    [0, focal_length, img_h / 2],
                                    [0, 0, 1]])
 
-            # The distortion coefficients
             distortion_matrix = np.zeros((4, 1), dtype=np.float64)
 
-            # SolvePnP
-            success,rotation_vec,translation_vec = cv2.solvePnP(face_3d,face_2d,cam_matrix,distortion_matrix)
-
-            # Getting rotational of face
-            rmat, jac = cv2.Rodrigues(rotation_vec)
-
-            
-            angles,mtxR,mtxQ,Qx,Qy,Qz = cv2.RQDecomp3x3(rmat)
+            success, rotation_vec, translation_vec = cv2.solvePnP(face_3d, face_2d, cam_matrix, distortion_matrix)
+            rmat, _ = cv2.Rodrigues(rotation_vec)
+            angles, _, _, _, _, _ = cv2.RQDecomp3x3(rmat)
 
             x = angles[0] * 360
             y = angles[1] * 360
             z = angles[2] * 360
 
-            # here based on axis rot angle is calculated
-            if y < -10:
-                text = "Looking Left"
-            elif y > 10:
-                text = "Looking Right"
-            elif x < -10:
+            if x < -10:
                 text = "Looking Down"
-            elif x > 10:
-                text = "Looking Up"
             else:
-                text = "Forward"
+                text = "Looking Up"
 
-            nose_3d_projection, jacobian = cv2.projectPoints(nose_3d, rotation_vec, translation_vec, cam_matrix, distortion_matrix)
+            print(text)
 
+            nose_3d_projection, _ = cv2.projectPoints(nose_3d, rotation_vec, translation_vec, cam_matrix, distortion_matrix)
             p1 = (int(nose_2d[0]), int(nose_2d[1]))
             p2 = (int(nose_2d[0] + y * 10), int(nose_2d[1] - x * 10))
 
             cv2.line(image, p1, p2, (255, 0, 0), 3)
-
             cv2.putText(image, text, (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 2)
             cv2.putText(image, "x: " + str(np.round(x, 2)), (500, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
             cv2.putText(image, "y: " + str(np.round(y, 2)), (500, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
             cv2.putText(image, "z: " + str(np.round(z, 2)), (500, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
         end = time.time()
-        totalTime = end - start
-
-        try:
-            fps = 1 / totalTime
-            print("FPS: ", fps)
-        except ZeroDivisionError:
-            fps = 0
-
-
+        fps = 1 / (end - start) if end - start > 0 else 0
         cv2.putText(image, f'FPS: {int(fps)}', (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 255, 0), 2)
 
         mp_drawing.draw_landmarks(image=image,
@@ -123,8 +84,10 @@ while cap.isOpened():
                                   connections=mp_face_mesh.FACEMESH_CONTOURS,
                                   landmark_drawing_spec=drawing_spec,
                                   connection_drawing_spec=drawing_spec)
+    
     cv2.imshow('Head Pose Detection', image)
-    if cv2.waitKey(5) & 0xFF == 27:
+    if cv2.waitKey(5) & 0xFF == ord('q'):
         break
-cap.release()
 
+cap.release()
+cv2.destroyAllWindows()
